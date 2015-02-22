fd92771dce7d75fed2f788d3a50d3bd7962f4324
Christian Fei <crifei93@gmail.com>
2015-02-22 11:01:41 +0100

    moved responsability to handle empty string to readInt

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -9,8 +9,8 @@ import Data.List.Split
 delimiters = [',','\n']
 
 stringCalc :: String -> Int
-stringCalc ""     = 0
 stringCalc n = sum $ map readInt $ splitWhen (\x -> x `elem` delimiters) n
 
 readInt :: String -> Int
-readInt = read
+readInt "" = 0
+readInt n = read n
