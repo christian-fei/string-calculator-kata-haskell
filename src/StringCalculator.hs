0a86df19b95d898f119f63e183eb3353dd6ff75b
Christian Fei <crifei93@gmail.com>
2015-02-22 11:08:05 +0100

    removed readInt

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -9,8 +9,5 @@ import Data.List.Split
 delimiters = [',','\n']
 
 stringCalc :: String -> Int
-stringCalc n = sum $ map readInt $ splitWhen (\x -> x `elem` delimiters) n
-
-readInt :: String -> Int
-readInt "" = 0
-readInt n = read n
+stringCalc "" = 0
+stringCalc n = sum $ map read $ splitWhen (\x -> x `elem` delimiters) n
