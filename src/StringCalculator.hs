5c5932b13f0c45393eda4445c271ed29401aa964
Christian Fei <crifei93@gmail.com>
2015-02-22 10:59:49 +0100

    simplified stringCalc

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -10,11 +10,7 @@ delimiters = [',','\n']
 
 stringCalc :: String -> Int
 stringCalc ""     = 0
-stringCalc n
-  | containsDelimiter     = sum $ map readInt $ splitWhen (\x -> x `elem` delimiters) n
-  | otherwise             = read n
-  where
-    containsDelimiter = or $ map (\x -> x `elem` delimiters) n
+stringCalc n = sum $ map readInt $ splitWhen (\x -> x `elem` delimiters) n
 
 readInt :: String -> Int
 readInt = read
