fea8ff139de9b8686e4fd2665ba0ef5930788948
Christian Fei <crifei93@gmail.com>
2015-02-25 14:33:14 +0100

    added 3d_attempt

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -1,2 +1,9 @@
+module StringCalculator
+where
 
+import Data.List.Split
 
+add :: String -> Int
+add "" = 0
+add xs = sum [read x | x <- splitWhen (==',') xs]
+-- add x = sum $ map read $ splitWhen (==',') x
--- src/StringCalculator.test.hs
+++ src/StringCalculator.test.hs
@@ -5,11 +5,30 @@ import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit
 
+import StringCalculator
+
 main = defaultMain tests
 
 tests = hUnitTestToTests $ TestList $
-  [alwaysFails]
+  singleNumbers ++ [
+    twoNumbersAreTheSum
+    ]
 
-alwaysFails =
-  True ~?= False
 
+singleNumbers =
+  [emptyListIsZero,
+  listOfNumberOneIsOne,
+  listOfNumberTwoIsTwo
+    ]
+
+emptyListIsZero =
+  add "" ~?= 0
+
+listOfNumberOneIsOne =
+  add "1" ~?= 1
+
+listOfNumberTwoIsTwo =
+  add "2" ~?= 2
+
+twoNumbersAreTheSum =
+  add "1,2" ~?= 3
